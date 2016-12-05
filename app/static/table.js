6aed901d3da5343fedd79257f29f2266241e717b
rafaelsmedina <r.salesmf@gmail.com>
2016-12-05 16:36:50 -0200

    Index page shows all pokemon

--- app/static/table.js
+++ app/static/table.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
-    $('#elections').DataTable( {
-        "ajax": {
-            "url": "/api/us_elections",
-            "dataSrc": "data"
-        }
+    $('#list-pokemon').DataTable( {
+        "ajax": "/api/pokemon",
+            "columns": [
+                {"data" : "num_dex" },
+                {"data" : "species" },
+                {"data" : "forme" },
+                {"data" : "type1" },
+                {"data" : "type2" },
+                {"data" : "ability1" },
+                {"data" : "ability2" },
+                {"data" : "abilityH" },
+                {"data" : "hp" },
+                {"data" : "attack" },
+                {"data" : "defense" },
+                {"data" : "spattack" },
+                {"data" : "spdefense" },
+                {"data" : "speed" },
+                {"data" : "total" },
+                {"data" : "pkmn_class" },
+                {"data" : "percent_male" },
+                {"data" : "percent_female" },
+
+            ]
     });
 });
